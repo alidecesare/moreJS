83208c8cefc535756eba61a88e28515589464ac5
Ali DeCesare <alidecesare@Alis-MBP.fios-router.home>
2020-04-07 12:11:04 -0400

    updated content

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("half-button").addEventListener("click", halfNumber);
+
+function halfNumber() {
+  var inputNumber = document.getElementById("half-input").value;
+  outputNumber = inputNumber/2;
+  alert("half of " + inputNumber + " is " + outputNumber);
+}
+
+function fortune() {
+  
+}
