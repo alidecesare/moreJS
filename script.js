34f1b304a65fdde104934d3ed6c2cfe1ca26229b
Ali DeCesare <alidecesare@Alis-MBP.fios-router.home>
2020-04-07 12:18:29 -0400

    updated content

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function halfNumber() {
   alert("half of " + inputNumber + " is " + outputNumber);
 }
 
+document.getElementById("fortune-button").addEventListener("click", fortune);
+
 function fortune() {
-  
+  var name = document.getElementById("fortune-input").value;
+  var fortuneOutput = document.getElementById("fortune-output").value;
+
+  var fortuneOutput = ["The early bird gets the worm, but the second mouse gets the cheese.", "Be on the alert to recognize your prime at whatever time of your life it may occur.", "Your road to glory will be rocky, but fulfilling.", "Courage is not simply one of the virtues, but the form of every virtue at the testing point."];
+
+  var randomFortune = fortuneOutput[Math.floor(Math.random()*fortuneOutput.length)];
+
+  alert(name + ", your fortune is: " + randomFortune);
 }
