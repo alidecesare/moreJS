6e82af3884e2a77deb3debcc9777d0a3ea9eb0ef
Ali DeCesare <alidecesare@Alis-MBP.fios-router.home>
2020-04-07 13:16:22 -0400

    updated content

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById("fortune-button").addEventListener("click", fortune);
 
 function fortune() {
   var name = document.getElementById("fortune-input").value;
-  var fortuneOutput = document.getElementById("fortune-output").value;
+  var fortuneOutput = document.getElementById("fortune-output-text").value;
 
   var fortuneOutput = ["The early bird gets the worm, but the second mouse gets the cheese.", "Be on the alert to recognize your prime at whatever time of your life it may occur.", "Your road to glory will be rocky, but fulfilling.", "Courage is not simply one of the virtues, but the form of every virtue at the testing point."];
 
@@ -18,3 +18,20 @@ function fortune() {
 
   document.getElementById("fortune-output").innerHTML = (name + ", your fortune is: " + randomFortune);
 }
+
+var fortuneOutput = document.getElementById("fortune-output-text");
+fortuneOutput.addEventListener("onclick", restyle)
+
+function restyle() {
+  var fontSize = Math.floor(Math.random() * 42);
+  var col1 = Math.floor(Math.random() * 255);
+  var col2 = Math.floor(Math.random() * 255);
+  var col3 = Math.floor(Math.random() * 255);
+  var bG = Math.floor(Math.random() * 255);
+
+  fortuneOutput.style.fontSize = fontSize + "px";
+  fortuneOutput.style.color = "rgb(" + col1 + "," + col2 + "," + col3 + ")";
+  fortuneOutput.style.background = "rgb(" + bG + "," + col2 + "," + col3 + ")";
+}
+
+restyle();
